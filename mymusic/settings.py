502d9da8f12eb233bbce1970a5f91fb99d8dcc96
Nirupam21 <36543804+Nirupam21@users.noreply.github.com>
2019-07-04 21:59:10 +0530

    Update settings.py

--- mymusic/settings.py
+++ mymusic/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'music/media')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 ]
